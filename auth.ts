ee37420b232a9ad2dad3f9209d01d056a274607b
azkriven <azkriven18@gmail.com>
2025-03-23 13:20:02 +0800

    ✨ feat: added next-auth with && google auth

--- auth.ts
+++ auth.ts
@@ -6,6 +6,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         GoogleProvider({
             clientId: process.env.AUTH_GOOGLE_ID!,
             clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
         }),
     ],
     secret: process.env.AUTH_SECRET,
