0c36ba1d00f26e4d386827c7f9529d095108d769
azkriven <azkriven18@gmail.com>
2025-03-23 16:00:15 +0800

    ✨ feat: sync users to database

--- auth.ts
+++ auth.ts
@@ -1,7 +1,15 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./db";
+import { accounts, sessions, users } from "./db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: DrizzleAdapter(db, {
+        usersTable: users,
+        accountsTable: accounts,
+        sessionsTable: sessions,
+    }),
     providers: [
         GoogleProvider({
             clientId: process.env.AUTH_GOOGLE_ID!,
